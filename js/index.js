d385c651fa0700a99ded2959fca0d1492855fe10
Takuya Takahashi <info@yujilow.net>
2016-06-18 20:18:06 +0900

    convert to html from markdown

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ function noteViewModel() {
   self.tag = ko.observable();
   self.posts = ko.observableArray([]);
   self.posts();
+  getPost(0,"root", function(data){
+    self.posts(convertPost(data));
+  });
 }
 
 ko.applyBindings(new noteViewModel());
@@ -10,6 +13,20 @@ ko.applyBindings(new noteViewModel());
 
 // functions
 
-function getPost(start, tag){
+function getPost(start, tag, callback){
+  $.ajax({
+    url:"http://localhost:3000/post?_start="+start+"&tag="+tag,
+    type:"GET"
+  }).done(function(data){
+    console.log(data[0])
+    callback(data);
+  });
+}
 
+function convertPost(data){
+  return data.map(function(e){
+    e.text = marked(e.text);
+    e.postedAt = new Date(e.postedAt);
+    return e;
+  });
 }
