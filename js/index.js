b566232f4c317ac87ecb5e68cfe0f6aabc88b952
Takuya Takahashi <info@yujilow.net>
2016-06-18 20:47:10 +0900

    apply card ui

--- js/index.js
+++ js/index.js
@@ -26,7 +26,34 @@ function getPost(start, tag, callback){
 function convertPost(data){
   return data.map(function(e){
     e.text = marked(e.text);
-    e.postedAt = new Date(e.postedAt);
+    e.postedAt = formatDate(new Date(e.postedAt), 'YYYY-MM-DD  hh:mm');
     return e;
   });
 }
+
+/*
+
+http://qiita.com/osakanafish/items/c64fe8a34e7221e811d0
+
+*/
+/**
+ * 日付をフォーマットする
+ * @param  {Date}   date     日付
+ * @param  {String} [format] フォーマット
+ * @return {String}          フォーマット済み日付
+ */
+var formatDate = function (date, format) {
+  if (!format) format = 'YYYY-MM-DD hh:mm:ss.SSS';
+  format = format.replace(/YYYY/g, date.getFullYear());
+  format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
+  format = format.replace(/DD/g, ('0' + date.getDate()).slice(-2));
+  format = format.replace(/hh/g, ('0' + date.getHours()).slice(-2));
+  format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2));
+  format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
+  if (format.match(/S/g)) {
+    var milliSeconds = ('00' + date.getMilliseconds()).slice(-3);
+    var length = format.match(/S/g).length;
+    for (var i = 0; i < length; i++) format = format.replace(/S/, milliSeconds.substring(i, i + 1));
+  }
+  return format;
+};
