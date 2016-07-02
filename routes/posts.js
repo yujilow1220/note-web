d4b0aa4a6904640afb05683869de05aed38b925f
Takuya Takahashi <info@yujilow.net>
2016-07-02 17:30:14 +0900

    set db and post

--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../lib/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/:start', function(req, res, next){
-  var start = req.params['start'];
-  res.send(start);
+router.post('/', function(req, res, next){
+  var post = new db.Post;
+  post.text = req.body.text;
+  post.save(function(err,data){
+    console.log(data);
+    res.send(data);
+  })
+});
+
+router.get('/test', function(req,res,next){
+  // var post = new db.Post;
+  // post.text = 'aaa';
+  // post.save();
+  db.Post.find({}, {}, function(err,doc){
+      console.log(err);
+      console.log(doc);
+      res.send(doc);
+  });
 });
 
 module.exports = router;
