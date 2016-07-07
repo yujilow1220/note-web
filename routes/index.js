89944f9fb0a7ec8495f07fd1a3cdd62af3b8239b
Takuya Takahashi <info@yujilow.net>
2016-07-07 09:34:03 +0900

    add sync func

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../lib/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/store', function(req, res, next){
+  var posts = req.body.posts;
+  console.log(req.body);
+  db.sync(posts, function(){
+      res.send("ok");
+  });
 });
 
 module.exports = router;
